544be5fa908bf9924e887e6c72e6605764ed8b3b
Victor Assis <victorassis545@gmail.com>
2018-11-24 17:18:02 -0200

    Mais rotas

--- app/routes/pdt/pdtRouter.js
+++ app/routes/pdt/pdtRouter.js
@@ -20,4 +20,24 @@ module.exports = function(app){
 				res.status(200).send({ pdt : resultSet });
 			}); //Query
 	});//Fim rota
+
+
+
+	app.get('/pdt/d/:id',function(req, res){
+
+		var idProfessor = req.params.id;
+
+			var sql = `select distinct(d.descricao) from professor_disciplina_turma as pdt
+	inner join disciplina as d on d.iddisciplina = pdt.iddisciplina
+	where idProfessor =` + idProfessor;
+
+			con.query(sql,function(err, resultSet, fields){
+				if(err){
+					throw err;
+				}
+
+				res.status(200).send({ disciplinas : resultSet });
+			}); //Query
+	});//Fim rota
+
 }
