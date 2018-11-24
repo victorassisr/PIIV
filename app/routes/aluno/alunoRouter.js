9cfffdb481db93851938c4f91a7d8a303e8cd8d5
Victor Assis <victorassis545@gmail.com>
2018-11-24 17:38:35 -0200

    Filtros

--- app/routes/aluno/alunoRouter.js
+++ app/routes/aluno/alunoRouter.js
@@ -6,13 +6,23 @@ module.exports = function(app){
 
 	app.get('/aluno',function(req, res){
 
-		var logged = true;
-
-		if(!logged){
-			res.status(400).send({err: "Usuário não logado!"});
-		}
-
 			var sql = "SELECT * FROM aluno";
+
+			con.query(sql,function(err, resultSet, fields){
+				if(err){
+					throw err;
+				}
+
+				res.status(200).send({ alunos : resultSet });
+			}); //Query
+	});//Fim rota
+
+
+	app.get('/aluno/turma/:id',function(req, res){
+
+		var id = req.params.id;
+
+			var sql = "SELECT * FROM aluno where turma = "+id;
 
 			con.query(sql,function(err, resultSet, fields){
 				if(err){
--- app/routes/homeRouter.js
+++ app/routes/homeRouter.js
@@ -1,3 +1,7 @@
+var conMysql = require('../../config/conMysql');
+
+var con = conMysql();
+
 module.exports = function(app){	
 	app.get('/',function(req, res){
 		res.status(200).send({ style: "Home" });
@@ -7,7 +11,16 @@ module.exports = function(app){
 
 		var notas = req.body;
 		for(i in notas){
-			console.log("ID: " + notas[i].id + " Nota: " + notas[i].nota);
+
+			var sql = "INSERT INTO teste(idAluno, nota) VALUES ('"+notas[i].id+"', '"+notas[i].nota+"')";
+
+			con.query(sql,function(err, resultSet, fields){
+				if(err){
+					throw err;
+				}
+
+				//res.status(200).send({ alunos : resultSet });
+			}); //Query
 		}
 		res.status(200).send({ req : req.body });
 	});
