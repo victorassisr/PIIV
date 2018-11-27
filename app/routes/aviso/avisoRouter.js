f130f0b89bce69fb85c6f5d6e29981675935a7b4
Victor Assis <victorassis545@gmail.com>
2018-11-27 00:57:49 -0200

    Alterações em aviso

--- app/routes/aviso/avisoRouter.js
+++ app/routes/aviso/avisoRouter.js
@@ -13,7 +13,52 @@ module.exports = function(app){
 					throw err;
 				}
 
-				res.status(200).send({ avisos : resultSet });
+				if(resultSet[0] == undefined){
+					res.status(200).send({ err : "Nenhum aviso encontrado!" });
+				} else{
+					res.status(200).send({ avisos : resultSet });
+				}
+			}); //Query
+	});//Fim rota
+
+
+
+	app.get('/aviso/turma/:idTurma',function(req, res){
+
+			var id = req.params.idTurma;
+
+			var sql = "SELECT * FROM aviso where idTurmaFechada = "+id;
+
+			con.query(sql,function(err, resultSet, fields){
+				if(err){
+					throw err;
+				}
+
+				if(resultSet[0] == undefined){
+					res.status(200).send({ err : "Nenhum aviso encontrado!" });
+				} else{
+					res.status(200).send({ avisos : resultSet });
+				}
+			}); //Query
+	});//Fim rota
+
+
+	app.get('/aviso/professor/:id',function(req, res){
+
+			var id = req.params.id;
+
+			var sql = "SELECT * FROM aviso where idAutor = "+id;
+
+			con.query(sql,function(err, resultSet, fields){
+				if(err){
+					throw err;
+				}
+
+				if(resultSet[0] == undefined){
+					res.status(200).send({ err : "Nenhum aviso encontrado!" });
+				} else{
+					res.status(200).send({ avisos : resultSet });
+				}
 			}); //Query
 	});//Fim rota
 }
--- app/routes/aviso/insereAvisoRouter.js
+++ app/routes/aviso/insereAvisoRouter.js
@@ -10,17 +10,29 @@ module.exports = function(app){
 
 		var aviso = req.body;
 
-		if(aviso.titulo != undefined && aviso.autor != undefined && aviso.corpo != undefined && aviso.dataPostagem != undefined){
+		if(aviso.titulo != undefined && aviso.autor != undefined && aviso.corpo != undefined && aviso.dataPostagem != undefined && aviso.idAutor != undefined && aviso.idTurmaFechada != undefined && aviso.idTipo){
 
-			var sql = "INSERT INTO aviso(titulo, autor, corpo, dataPostagem) VALUES('"+aviso.titulo+"','"+aviso.autor+"','"+aviso.corpo+"','"+aviso.dataPostagem+"')";
+			if(aviso.idTipo == 1){
+				var sql = "INSERT INTO aviso(titulo, idAutor, autor, corpo, dataPostagem, idTurmaFechada) VALUES('"+aviso.titulo+"','"+aviso.idAutor+"',"+aviso.autor+"','"+aviso.corpo+"','"+aviso.dataPostagem+"','"+aviso.idTurmaFechada+"')";
 
-			con.query(sql, function(err, success, fields){
-				if(err){
-					throw err;
-				}
+				con.query(sql, function(err, success, fields){
+					if(err){
+						throw err;
+					}
 
-				res.status(200).send({ aviso : aviso });
-			}); //Query
+					res.status(200).send({ aviso : aviso });
+				}); //query
+			} else{
+				var sql = "INSERT INTO aviso(titulo, idAutor, autor, corpo, dataPostagem, idTurmaFechada) VALUES('"+aviso.titulo+"','"+aviso.idAutor+"',"+aviso.autor+"','"+aviso.corpo+"','"+aviso.dataPostagem+"','0')";
+
+				con.query(sql, function(err, success, fields){
+					if(err){
+						throw err;
+					}
+
+					res.status(200).send({ aviso : aviso });
+				}); //query
+			}
 
 		} else {
 			res.status(200).send({ err : "O formato informado não é compativel!" });
